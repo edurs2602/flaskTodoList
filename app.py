0dfd5f451fc259be1d0d8bdfe4a9f7413b03109d
edurs2602 <edurs.2602@gmail.com>
2024-03-29 22:40:20 -0300

    feat: transicao completa para o postrgres e criação da aba user

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import render_template
+from flask import render_template, send_from_directory
 
 from src import db, app
 from src import todo, user
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+# Serve React app
+@app.route('/static/<path:path>')
+def serve_static(path):
+    return send_from_directory('frontend/todolist/build/static', path)
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return send_from_directory('frontend/todolist/build', 'index.html')
+
 
 if __name__ == '__main__':
     with app.app_context():
